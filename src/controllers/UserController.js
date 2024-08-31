1dccff21df197b886dbfbbd062cb4caa3de631b6
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-31 11:45:40 +0700

    login success

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -7,13 +7,33 @@ const register = asyncHandler(async (req, res) => {
       succuess: false,
       mes: "Missing inputs",
     });
-    const response = await User.create(req.body)
-    return res.status(200).json({
-      success: response ? true : false,
-      response
-    })
+  const user = await User.findOne({ email });
+  if (user) throw new Error("User existed");
+  const newUser = await User.create(req.body);
+  return res.status(200).json({
+    success: user ? true : false,
+    mes: newUser ? "Register successfully!" : "Something went wrong!",
+    data: newUser
+  });
 });
-
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({
+      succuess: false,
+      mes: "Missing inputs",
+    });
+  const response = await User.findOne({ email });
+  if (response && await response.isCorrectPassword(password)) {
+    return res.status(200).json({
+      sucess: true,
+      data: response
+    });
+  } else {
+    throw new Error("Incorrect email or passord!");
+  }
+});
 module.exports = {
   register,
+  login,
 };
