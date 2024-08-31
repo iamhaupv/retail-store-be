57860c369a5f12649b71dda4c4263a9ac32ae2c2
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-31 10:39:49 +0700

    isModified

--- src/models/User.js
+++ src/models/User.js
@@ -54,6 +54,9 @@ var userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
   const salt = bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
