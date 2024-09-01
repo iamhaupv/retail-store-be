81158aa7a06acfdb814e9c459eed5da2237e2db7
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-09-01 15:14:54 +0700

    comment descrip user router:

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -2,15 +2,26 @@ const express = require("express");
 const router = express.Router();
 const { UserController } = require("../controllers/index");
 const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
+// register user
 router.post("/register", UserController.register);
+//  login user
 router.post("/login", UserController.login);
+// get user
 router.get("/current", verifyAccessToken, UserController.getCurrent);
+// refresh token
 router.post("/refresh-token", UserController.refreshAccessToken);
+// log out user
 router.get("/logout", UserController.logout);
+// forgot password
 router.post("/forgotpassword", UserController.forgotPassword);
+// reset password
 router.put("/resetpassword", UserController.resetPassword);
+//  get all user
 router.get("/", [verifyAccessToken, isAdmin], UserController.getUsers);
+// delete user
 router.delete("/", [verifyAccessToken, isAdmin], UserController.deleteUser);
+// update user
 router.put("/current", verifyAccessToken, UserController.updateUser);
-router.put("/:uid", [verifyAccessToken, isAdmin], UserController.updateUser);
+// update user
+router.put("/:uid", [verifyAccessToken, isAdmin], UserController.updateUserByAdmin);
 module.exports = router;
