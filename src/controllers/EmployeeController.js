d34adab7460c90f54c80cb92b7c4ffe7a462906c
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-10-01 13:56:05 +0700

    get list employees

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -1,25 +1,36 @@
-const {Employee} = require("../models/index")
-// create employee 
-const createEmployee = async(req, res) => {
-    try {
-        if(!req.body || Object.keys(req.body).length === 0) return res.status(400).json({
-            success: false,
-            mes: "Missing input!"
-        })
-        const {email} = req.body
-        const employeeExist = await Employee.findOne({email: email})
-        if(employeeExist){
-            throw new Error("Cannot create new employee!")
-        }
-        const newEmployee = await Employee.create(req.body)
-        return res.status(200).json({
-            success: newEmployee ? true : false,
-            data: newEmployee ? newEmployee : "Cannot create new employee"
-        })
-    } catch (error) {
-        throw new Error(error)
+const expressAsyncHandler = require("express-async-handler");
+const { Employee } = require("../models/index");
+// create employee
+const createEmployee = expressAsyncHandler( async(req, res) => {
+    if (!req.body || Object.keys(req.body).length === 0)
+      return res.status(400).json({
+        success: false,
+        mes: "Missing input!",
+      });
+    const { email } = req.body;
+    const employeeExist = await Employee.findOne({ email: email });
+    if (employeeExist) {
+      throw new Error("Cannot create new employee!");
     }
-}
+    const newEmployee = await Employee.create(req.body);
+    return res.status(200).json({
+      success: newEmployee ? true : false,
+      data: newEmployee ? newEmployee : "Cannot create new employee",
+    });
+});
+// get list employee
+const getListEmployee = expressAsyncHandler(async (req, res) => {
+    try {
+      const employees = await Employee.find();
+      return res.status(200).json({
+        success: employees ? true : false,
+        data: employees ? employees : "Cannot get list employees!",
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  })
 module.exports = {
-    createEmployee
-}
+  createEmployee,
+  getListEmployee,
+};
--- src/routes/EmployeeRouter.js
+++ src/routes/EmployeeRouter.js
@@ -3,4 +3,5 @@ const router = express.Router()
 const {verifyAccessToken, isAdmin} = require("../middlewares/verifyToken")
 const {EmployeeController} = require("../controllers/index")
 router.post("/", [verifyAccessToken, isAdmin], EmployeeController.createEmployee)
+router.get("/", [verifyAccessToken, isAdmin], EmployeeController.getListEmployee)
 module.exports = router
