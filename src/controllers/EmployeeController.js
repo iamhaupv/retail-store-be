a1fe9d1178a3d675558463e2c97e998fb22c838c
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-10-13 08:26:01 +0700

    update create employee

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -12,6 +12,10 @@ const createEmployee = expressAsyncHandler( async(req, res) => {
     if (employeeExist) {
       throw new Error("Cannot create new employee!");
     }
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ success: false, message: "Missing input files!" });
+    }
+    req.body.images = req.files.map(el => el.path);
     const newEmployee = await Employee.create(req.body);
     return res.status(200).json({
       success: newEmployee ? true : false,
--- src/routes/EmployeeRouter.js
+++ src/routes/EmployeeRouter.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const {verifyAccessToken, isAdmin} = require("../middlewares/verifyToken")
 const {EmployeeController} = require("../controllers/index")
-router.post("/", [verifyAccessToken, isAdmin], EmployeeController.createEmployee)
+const uploader = require("../config/cloudinary.config")
+router.post("/", [verifyAccessToken, isAdmin], uploader.array("images", 10),EmployeeController.createEmployee)
 router.get("/", [verifyAccessToken, isAdmin], EmployeeController.getListEmployee)
 module.exports = router
